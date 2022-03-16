806474b40f2cfd3c5dfa3e8068227ab4f652adcc
Kaligu Jayanath <101045808+kaligu@users.noreply.github.com>
2022-03-16 19:01:49 +0530

    Create print perimeter and area using def.py

---
+++ print perimeter and area using def.py
@@ -0,0 +1,11 @@
+#print perimeter and area using def.py
+
+#to print perimeter and area input as length=5 and width=5 *using def functions
+
+def func(length,width):
+        perimeter = 2*(length + width)
+        area = length*width
+        print("perimeter is " ,perimeter)
+        print('area is ' ,area)
+
+func(5,5)
