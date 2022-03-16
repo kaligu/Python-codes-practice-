f3330c71ef6960037d33ad494185483e4e83dc8b
Kaligu Jayanath <101045808+kaligu@users.noreply.github.com>
2022-03-16 19:05:40 +0530

    Create print using circle input radius print circumference and area.py

---
+++ print using circle input radius print circumference and area.py
@@ -0,0 +1,14 @@
+#print using circle input radius print circumference and area.py
+
+#in the circle
+#          input radius
+#          output circumference (2*pi*r) and area (pi*r*r)
+#          with def and return
+
+r = int(input())
+def cal(radius):
+        pi=3.14
+        circumference = 2*pi*radius
+        area = pi*radius*radius
+        return print('circumference of ' , circumference ,'area of ' , area)
+Cal ( r )
